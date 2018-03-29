0a15c1e806d31d187dbbff8d0c4f362e18bef8d7
Dobi Okrasa <dobi.okrasa@gmail.com>
2018-03-29 19:22:40 +0200

    Add script for inside linking

--- scripts.js
+++ scripts.js
@@ -7,4 +7,19 @@ $(function () {
     autoPlay: 3000,
     wrapAround: true
   });
+
+  function offsetAnchor() {
+    if (location.hash.length !== 0) {
+      window.scrollTo(window.scrollX, window.scrollY - 95);
+    }
+  }
+   
+  $(document).on('click', 'a[href^="#"]', function(event) {
+    window.setTimeout(function() {
+      offsetAnchor();
+    }, 0);
+  });
+  
+  window.setTimeout(offsetAnchor, 0);
+
 });
