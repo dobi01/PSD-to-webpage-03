e53df338cb4a0be2750c90fc144147d7827d232f
Dobi Okrasa <dobi.okrasa@gmail.com>
2018-04-02 17:32:07 +0200

    Add navigation for  windowMaxWidth1200px

--- scripts.js
+++ scripts.js
@@ -1,6 +1,8 @@
 $(function () {
   $('body').removeClass('fade-out');
 
+
+  // CAROUSELS
   $('.main-carousel').flickity({
     cellAlign: 'center',
     contain: true,
@@ -22,4 +24,42 @@ $(function () {
   
   window.setTimeout(offsetAnchor, 0);
 
+  // MEDIA QUERY
+  var windowMaxWidth1200px = window.matchMedia("(max-width: 1200px)");
+  
+  function mediaQueryFunction(x) {
+    if (x.matches) { 
+      $('nav ul').css('display', 'none');
+
+      $('#menu-button').click(function () {
+        let isVisible = $('nav ul').is(':visible');
+        if (isVisible) {
+          $('nav ul').fadeOut('fast');
+        } else {
+          $('nav ul').fadeIn('fast');
+        }
+      });
+    
+      $('nav ul li a').click(function () {  
+        $('nav ul').fadeOut('fast');
+      });
+
+    } else {
+      $('nav ul').css('display', 'block');
+      
+      // $('nav ul li a').click(function () {  
+      //   $('nav ul').css('display', 'block');
+      //   // let isVisible = $('nav ul').is(':visible');
+      //   // if (isVisible) {
+      //   //   $('nav ul').fadeIn('15000');
+      //   // } else {
+      //   //   $('nav ul').css('display', 'block');
+      //   // }
+      // });
+    }
+  }
+
+  windowMaxWidth1200px.addListener(mediaQueryFunction);
+  mediaQueryFunction(windowMaxWidth1200px);
+ 
 });
