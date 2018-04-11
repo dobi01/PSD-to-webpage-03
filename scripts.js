89aba48672d398b1a455ba052c72afad58048a96
Dobi Okrasa <dobi.okrasa@gmail.com>
2018-04-11 23:30:25 +0200

    Fix carousels and navigation, mobile scroll

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+/*jshint esversion: 6 */
+
 $(function () {
   $('body').removeClass('fade-out');
 
@@ -10,28 +12,50 @@ $(function () {
   });
 
   // NAV LINKS
-  $(document).on('click', 'a[href^="#"]', function(event) {
-    $('body, html').animate({
-      scrollTop: $($.attr(this, 'href')).offset().top - 95
-    }, 1500);
-  });
+  function scrollFlow(x) {
+    $(document).on('click', 'a[href^="#"]', function(event) {
+      $('body, html').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top - x
+      }, 1500);
+    });
+  }
 
   // MEDIA QUERY
-  var windowMaxWidth1450px = window.matchMedia("(max-width: 1450px)");
-  var windowMaxWidth1350px = window.matchMedia("(max-width: 1350px)");
-  var windowMaxWidth1250px = window.matchMedia("(max-width: 1250px)");
-  var windowMaxWidth1200px = window.matchMedia("(max-width: 1200px)");
-  
+  const windowMaxWidth1450px = window.matchMedia("(max-width: 1450px)"),
+        windowMaxWidth1350px = window.matchMedia("(max-width: 1350px)"),
+        windowMaxWidth1250px = window.matchMedia("(max-width: 1250px)"),
+        windowMaxWidth1200px = window.matchMedia("(max-width: 1200px)"),
+        carPremium = $('#premium .main-carousel'),
+        carTeam = $('#team .main-carousel'),
+        carClients = $('#clients .main-carousel'),
+        offset95 = 95,
+        offset40 = 40,
+        navUl = $('nav ul'),
+        menuButton = $('#menu-button'),
+        navLinks = $('nav ul li a');
+
   function mediaQuery1450px(x) {
-    if (x.matches) $('#premium .main-carousel').flickity( 'destroy' );
+    if (x.matches) {
+      carPremium.flickity('destroy');
+    } else {
+      carPremium.flickity();
+    }
   }
 
   function mediaQuery1350px(x) {
-    if (x.matches) $('#team .main-carousel').flickity( 'destroy' );
+    if (x.matches) {
+      carTeam.flickity('destroy');
+    } else {
+      carTeam.flickity();
+    }
   }
 
   function mediaQuery1250px(x) {
-    if (x.matches) $('#clients .main-carousel').flickity( 'destroy' );
+    if (x.matches) {
+      carClients.flickity('destroy');
+    } else {
+      carClients.flickity();
+    }
   }
 
   windowMaxWidth1450px.addListener(mediaQuery1450px);
@@ -43,35 +67,37 @@ $(function () {
   windowMaxWidth1250px.addListener(mediaQuery1250px);
   mediaQuery1250px(windowMaxWidth1250px);
 
-
   $(window).on('resize', function() {
     let windowWidth= $(this).width();
-    if (windowWidth <= 1450) $('#premium .main-carousel').flickity( 'destroy' );
-    if (windowWidth <= 1350) $('#team .main-carousel').flickity( 'destroy' );
-    if (windowWidth <= 1250) $('#clients .main-carousel').flickity( 'destroy' );
+    if (windowWidth <= 1450) carPremium.flickity('destroy');
+    if (windowWidth <= 1350) carTeam.flickity('destroy');
+    if (windowWidth <= 1250) carClients.flickity('destroy');
   });
-
-
 
   function mediaQuery1200px(x) {
     if (x.matches) { 
-      $('nav ul').css('display', 'none');
 
-      $('#menu-button').click(function () {
-        let isVisible = $('nav ul').is(':visible');
+      scrollFlow(offset40);
+      navUl.addClass('invisible'); 
+
+      menuButton.click(function() { 
+        let isVisible = navUl.is(':visible');
         if (isVisible) {
-          $('nav ul').fadeOut('fast');
+          navUl.fadeOut('fast');
         } else {
-          $('nav ul').fadeIn('fast');
+          navUl.fadeIn('fast');
         }
       });
-    
-      $('nav ul li a').click(function () {  
-        $('nav ul').fadeOut('fast');
+
+      navLinks.click(function () {   
+        navUl.fadeOut('fast');
       });
 
     } else {
-      $('nav ul').css('display', 'inline-block');
+      navUl.css('display', 'inline-block');
+      navLinks.off('click');
+      menuButton.off('click');
+      scrollFlow(offset95);
     }
   }
 
